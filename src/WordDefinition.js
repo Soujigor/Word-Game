d041accec45dfceed92fedbd50689cf157892958
Yago Batista <yagobatistafis@yahoo.com.br>
2022-02-06 16:07:50 -0300

    wordDef modal update

--- src/WordDefinition.js
+++ src/WordDefinition.js
@@ -1,40 +1,87 @@
 import React, { useState } from "react";
 import { WordState } from "./Context";
 import "./WordDefinition.css";
+import { Text, Grid, GridItem, HStack } from "@chakra-ui/react";
 
 const WordDefinition = (props) => {
   const { state, dispatch } = WordState();
   const [foo, setFoo] = useState(false);
   return (
-    <div className="word-def-container">
-      <h1
-        className="right-ans"
-        onMouseEnter={() => setFoo(true)}
-        onMouseLeave={() => setFoo(false)}
-      >
-        {state.rightAns.join("")}
-      </h1>
-      {foo && <h1>deu certo, otário</h1>}
+    // <div className="word-def-container">
+    //   <Text
+    //     className="right-ans"
+    //     onMouseEnter={() => setFoo(true)}
+    //     onMouseLeave={() => setFoo(false)}
+    //   >
+    //     {state.rightAns.join("")}
+    //   </Text>
+    //   {foo && <Text>deu certo, otário</Text>}
+    //   {props.word.map((wordDef) => (
+    //     <div>
+    //       <Text>{wordDef.fl}</Text>
+    //       <div
+    //         className="synonyms"
+    //         onMouseEnter={() => setFoo(true)}
+    //         onMouseLeave={() => setFoo(false)}
+    //       >
+    //         Synonyms:
+    //         {foo && (
+    //           <Text>
+    //             {wordDef.meta.syns[0].map((syn) => (
+    //               <Text>{syn}</Text>
+    //             ))}
+    //           </Text>
+    //         )}
+    //       </div>
+    //       <div className="shortDef">
+    //         Short Definition:
+    //         {wordDef.shortdef.map((shortdef) => (
+    //           <Text>{shortdef}</Text>
+    //         ))}
+    //       </div>
+    //     </div>
+    //   ))}
+    // </div>
+
+    <Grid templateColumns="1fr 3fr 1fr" gap={1} color="#c0f7b7">
+      <GridItem>
+        <Text>Correct Answer</Text>
+      </GridItem>
+      <GridItem colStart={2} colEnd={-1}>
+        <Text textAlign="center">{state.rightAns.join("")}</Text>
+      </GridItem>
       {props.word.map((wordDef) => (
-        <div>
-          <h1>{wordDef.fl}</h1>
-          <div
-            className="synonyms"
-            onMouseEnter={() => setFoo(true)}
-            onMouseLeave={() => setFoo(false)}
-          >
-            Synonyms:
-            {foo && wordDef.meta.syns[0].map((syn) => <h4>{syn}</h4>)}
-          </div>
-          <div className="shortDef">
-            Short Definition:
+        <>
+          <GridItem colStart={1} colEnd={-1}>
+            <Text bg="red" textAlign="center">
+              {wordDef.fl.toUpperCase()}
+            </Text>
+          </GridItem>
+          <GridItem>
+            <Text>Synonyms</Text>
+          </GridItem>
+          <GridItem bg="red" colStart={2} colEnd={-1}>
+            <HStack>
+              {wordDef.meta.syns[0].map((syn, index) =>
+                index != wordDef.meta.syns[0].length - 1 ? (
+                  <Text bg="green">{`${syn},`}</Text>
+                ) : (
+                  <Text>{syn}</Text>
+                )
+              )}
+            </HStack>
+          </GridItem>
+          <GridItem>
+            <Text>Short Definition</Text>
+          </GridItem>
+          <GridItem colStart={2} colEnd={-1}>
             {wordDef.shortdef.map((shortdef) => (
-              <h5>{shortdef}</h5>
+              <Text>{shortdef}</Text>
             ))}
-          </div>
-        </div>
+          </GridItem>
+        </>
       ))}
-    </div>
+    </Grid>
   );
 };
 
